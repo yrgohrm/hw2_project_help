72d91b987e99a0c3fdf233b2d5142b49918d47b5
Hampus Ram <hampus.ram@educ.goteborg.se>
2023-07-06 14:11:55 +0200

    add github cli

--- setup-new-pi.sh
+++ setup-new-pi.sh
@@ -2,8 +2,19 @@
 
 # This script is used to setup a new Raspberry Pi with the necessary stuff
 
+# install apt packages
+
 sudo apt update
 sudo apt upgrade -y
 
 sudo apt install raspberrypi-kernel-headers python3-pip python3-venv git -y
 sudo apt install cmake libgtest-dev libgpiod-dev -y
+
+# install GitHub CLI
+
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
